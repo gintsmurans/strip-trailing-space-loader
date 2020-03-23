b6bd2b786957f342b97c1fc37ada730005ed025a
Gints Murans <gm@gm.lv>
2020-03-23 20:31:06 +0200

    Added option to change tabs to spaces (default action)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const loaderUtils = require('loader-utils');
 
 const defaultOptions = {
     line_endings: 'unix',
+    spacing: 'space',
 };
 
 module.exports = function (content) {
@@ -13,6 +14,9 @@ module.exports = function (content) {
         let result = content;
         if (loaderOptions.line_endings === 'unix') {
             result = result.replace(/\r/g, ''); // Remove CR characters, git on linux is not very found of them
+        }
+        if (loaderOptions.spacing === 'space') {
+            result = result.replace(/^[\t]*/gm, '    ') // Replace all starting tabs with spaces
         }
         result = result
             .replace(/ *import/g, 'import') // Fix issue where developer has prepended import statement with a space.
